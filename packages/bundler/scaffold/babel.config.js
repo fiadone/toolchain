1fb9a2ef1fa045010f2b4609af9d7417140116d5
fiadone <hello@fiad.one>
2020-04-20 21:01:55 +0200

    [bundler] decorators plugin fix

--- packages/bundler/scaffold/babel.config.js
+++ packages/bundler/scaffold/babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-decorators',
+    ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-private-methods'
