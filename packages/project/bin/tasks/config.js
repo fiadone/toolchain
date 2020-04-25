875574e7a798e2a71f1faaa1e62742e55ed923b3
fiadone <hello@fiad.one>
2020-04-25 18:37:14 +0200

    [project] test coverage and gitignore file

--- packages/project/bin/tasks/config.js
+++ packages/project/bin/tasks/config.js
@@ -1,7 +1,7 @@
 const { addConfig } = require('@fiad/cli')
 
 module.exports = async function () {
-  await addConfig('test', 'jest', 'scripts', true)
+  await addConfig('test', 'jest --coverage', 'scripts', true)
   await addConfig('prettify', 'prettier', 'scripts', true)
   await addConfig('browserslist', '["defaults"]')
 }
