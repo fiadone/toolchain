8d87b9055b108b2b90d406cdb8004a2bd0bdae42
fiadone <hello@fiad.one>
2020-04-25 18:40:36 +0200

    [toolbox/math] test update

--- packages/toolbox/math/math.test.js
+++ packages/toolbox/math/math.test.js
@@ -1,16 +1,26 @@
 import { map, clamp, lerp } from '@fiad/toolbox/math'
 
-test('Should map a value between two ranges', () => {
+test('It should map the value across the two given ranges', () => {
   const result = map(8, [6, 10], [18, 30])
   expect(result).toBe(24)
 })
 
-test('Should clamp a value between two bounds', () => {
+test('It should map the value across the two given values', () => {
+  const result = map(5, 10, 100)
+  expect(result).toBe(50)
+})
+
+test('It should clamp the value across the two given bounds', () => {
   const result = clamp(2, 0, 1)
   expect(result).toBe(1)
 })
 
-test('Should linearly interpolate two values', () => {
+test('It should return the linear interpolant between the two given values (with default factor)', () => {
+  const result = lerp(0, 10)
+  expect(result).toBe(10)
+})
+
+test('It should return the linear interpolant between the two given values (with custom factor)', () => {
   const result = lerp(0, 10, 0.5)
   expect(result).toBe(5)
 })
