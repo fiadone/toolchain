fd7bcf4639ed5ad8ef7ce263d1589554c9bab07d
fiadone <hello@fiad.one>
2020-04-25 22:09:45 +0200

    [server] conditional dev script

--- packages/server/bin/tasks/config.js
+++ packages/server/bin/tasks/config.js
@@ -1,6 +1,13 @@
+const fs = require('fs-extra')
+const path = require('path')
 const { addConfig } = require('@fiad/cli')
 
 module.exports = async function () {
-  await addConfig('dev', '"node server.js --env development"', 'scripts', true)
+  const bundlerConfig = path.join(process.cwd(), 'bundler.config.js')
+
+  if (fs.existsSync(bundlerConfig)) {
+    await addConfig('dev', '"node server.js --env development"', 'scripts', true)
+  }
+
   await addConfig('start', '"node server.js --env production"', 'scripts', true)
 }
