525f60769ccff3278141902bcc4240548afc51c1
fiadone <hello@fiad.one>
2020-04-25 18:49:38 +0200

    [toolbox] full library export fix

--- packages/toolbox/index.js
+++ packages/toolbox/index.js
@@ -3,12 +3,13 @@
  * @description A smart collection of front-end development tools and utilities
  */
 
-export { default as detect } from './detect'
-export { default as dom } from './dom'
+export * as cache from './cache'
+export * as detect from './detect'
+export * as dom from './dom'
 export { default as EventsManager } from './events'
-export { default as math } from './math'
+export * as math from './math'
 export { default as ShareManager } from './share'
-export { default as SmoothScroll } from './scroll'
+export { default as SmoothScroll } from './smooth-scroll'
 export { default as Store } from './store'
 export { default as Stream } from './stream'
-export { default as strings } from './strings'
+export * as strings from './strings'
