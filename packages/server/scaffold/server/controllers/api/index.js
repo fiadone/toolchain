d9b674716a3ab15ee44128cfbe16e2869a9647c8
fiadone <hello@fiad.one>
2020-05-06 09:23:14 +0200

    [server] cors support

--- packages/server/scaffold/server/controllers/api/index.js
+++ packages/server/scaffold/server/controllers/api/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
+const cors = require('cors')
 const registerRoutes = require('./routes')
 
 const router = Router()
 
+router.use(cors())
 registerRoutes(router)
 
 module.exports = function () {
