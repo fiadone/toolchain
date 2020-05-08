5724bc148747b16d1d7d0c05949a6289586efa47
fiadone <hello@fiad.one>
2020-05-08 12:26:14 +0200

    [toolbox/detect] browser, engine and os detection

--- packages/toolbox/detect/index.js
+++ packages/toolbox/detect/index.js
@@ -4,9 +4,28 @@
  * @description A simple collector of device info
  */
 
-import detect from 'ismobilejs'
+import parseUserAgent from 'ua-parser-js'
+import getMobileInfo from 'ismobilejs'
 
-const { windows, amazon, other, ...mobile } = detect(navigator.userAgent)
-const touch = 'touchstart' in window
+/**
+ * Collects device info
+ * @param {string} userAgent The user agent string
+ * @returns {object}
+ */
+function detect(userAgent = navigator.userAgent) {
+  const { browser, engine, os } = parseUserAgent(userAgent)
+  const { windows, amazon, other, ...mobile } = getMobileInfo(userAgent)
+  const info = { browser, engine, mobile, os }
 
-export { mobile, touch }
+  // prevent errors in ssr
+  if (typeof window !== 'undefined') {
+    info.touch = 'touchstart' in window
+    // adding modern flag according to CSS.supports API support
+    info.browser.modern = 'CSS' in window && 'supports' in window.CSS
+  }
+
+  return info
+}
+
+export default detect()
+export { detect }
